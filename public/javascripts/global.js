b133f78a4192bd69408cdb4472424f2ad015643f
1M0reBug <jordan.quagliatini@gmail.com>
2015-04-13 23:47:13 +0200

    Post works, I can add an article on the blog :+1:, but redirection doesn't works

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -7,16 +7,39 @@ var App = angular.module('App', []);
 	App.controller("showArticles", ['$scope', '$http', function($scope, $http){
 
 		$scope.getArticles = function() {
-			console.log("$scope.getArtcles appelée ! ");
 			$http.get( '/articles/articles' )
 				.then(function(response){
 					$scope.articles = response.data;
-					console.log("data:");
-					console.log(data);
 				});
 		};
 
 		$scope.getArticles();
 
+	}]);
+
+
+	App.controller("addArticle", ['$scope', '$http', function($scope, $http){
+
+		$scope.article = {
+			title : "",
+			author : "",
+			body : ""
+		};
+
+		$scope.sendArticle = function(){
+			var art = $scope.article;
+			if(art.body !== "" && art.author !== "" && art.title !== ""){
+				$http.post('/articles/new', $scope.article)
+					.success(function(response){
+						window.location = 'http://localhost:3000/'; 
+					})
+					.error(function(response, status){
+						$scope.error = {message : response.msg};
+					});
+			}
+			else{
+				$scope.error = {message : "All fields are not full !"};
+			}
+		};
 	}]);
 })();
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,5 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'My Awesome blog !' });
 });
 
+
 module.exports = router;
