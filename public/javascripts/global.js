9a5693edbedcc330773b1618ba7d99c361d51e4b
1M0reBug <jordan.quagliatini@gmail.com>
2015-04-26 17:59:48 +0200

    comit before -1

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -4,18 +4,44 @@ var App = angular.module('App', []);
 
 	'use strict';
 
-	App.controller("showArticles", ['$scope', '$http', function($scope, $http){
+	App.config(['$locationProvider', function($locationProvider){
+		$locationProvider.html5Mode({enabled:true, requireBase : false});
+	}]);
 
-		$scope.getArticles = function() {
-			$http.get( '/articles/articles' )
-				.then(function(response){
-					$scope.articles = response.data;
-				});
-		};
+	App.controller("showArticles", ['$scope', '$http','$location',
+		function($scope, $http,$location){
+			$scope.getArticles = function(){
+				$scope.numberOfPages = 0;
+				$http.get('/articles/count')
+					.then(function(response){
+						var tenth = Math.ceil(response.data / 10);
+						$scope.pagination = {
+							numberOfPages: new Array(tenth),
+							currentPage: $location.search().page || 1
+						};
+						var _this = $scope.pagination;
+						$scope.pagination.currentIsFirst = _this.currentPage <= 1;
+
+						$scope.pagination.currentIsLast =  _this.currentPage >= _this.numberOfPages.length;
+
+						var numberOfPages = _this.numberOfPages.length;
+						var page = _this.currentPage;
+						page = page > numberOfPages?numberOfPages:page;
+						page = page <=1?1:page;
+						var url = page > 1 ? '/articles/articles?page='+page:'/articles/articles';
+						$http.get( url )
+							.then(function(response){
+								$scope.articles = response.data;
+							});
+					});
+
+
+			};
 
 		$scope.getArticles();
 
 	}]);
+
 
 
 	App.controller("addArticle", ['$scope', '$http', function($scope, $http){
@@ -31,7 +57,7 @@ var App = angular.module('App', []);
 			if(art.body !== "" && art.author !== "" && art.title !== ""){
 				$http.post('/articles/new', $scope.article)
 					.success(function(response){
-						window.location = 'http://localhost:3000/'; 
+						window.location.replace('/'); 
 					})
 					.error(function(response, status){
 						$scope.error = {message : response.msg};
@@ -42,4 +68,7 @@ var App = angular.module('App', []);
 			}
 		};
 	}]);
+
+
+
 })();
--- routes/articles.js
+++ routes/articles.js
@@ -6,15 +6,46 @@ var router = express.Router();
 router.get('/articles', function(req, res){
 
 	var db = req.db;
-	db.collection('articles').find().sort({date:1}).toArray(function(err, items){
+
+	var limit = 10;
+	var count = getCount(db);
+
+	var numberOfPage = Math.ceil(count/limit);
+
+	//On laisse la possibilité de la pagination
+	var page = req.query.page;
+	page = page > numberOfPage?numberOfPage:page;
+	page = page<=1?1:page;
+
+	var queryString = "{";
+	if ( req.query.title !== null){
+
+	}
+
+	db.collection('articles').find().skip(page > 0?((page-1)*10):0).limit(limit).sort({date:-1}).toArray(function(err, items){
 		res.json(items);
 	});
+});
+
+/* GET Total number of articles */
+router.get('/count', function(req,res){
+	var db = req.db;
+	res.json(getCount(db));
+
 });
 
 /* GET new article view */
 router.get('/new', function(req, res){
 	res.render('new', {title: "My Awesome blog !"});
 });
+
+router.get('/:id', function (req, res) {
+	var db = req.db;
+
+	db.collection('articles').find({"_id" : "ObjectId("+id+")"}).toArray(function (err, items) {
+		res.render('article', items);
+	});
+})
 
 /* POST new article to mongo */
 router.post("/new", function(req, res){
@@ -51,6 +82,12 @@ var now = function(){
 	seconds = seconds<10?"0"+seconds:seconds;
 
 	return month + "/" + day + "/" + year + " " + hour + ":" + minutes + ":" + seconds;
+}
+
+var getCount = function (db) {
+	db.collection('articles').count(function (err, items) {
+		return items;
+	});
 };
 
 module.exports = router;
